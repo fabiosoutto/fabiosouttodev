f0ae100a49b7d5a3e8bd28b687d32f090ea027ea
FabioSoutto <fgimenes905@gmail.com>
2022-08-22 14:00:31 -0300

    edit about me

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ function backToTop() {
 // código do texto animado data-show
 setInterval(function () {
     const show = document.querySelector('span[data-show]')
-    const next = show.nextElementSibling || document.querySelector('span:first-child')
+    const next = show.nextElementSibling || document.querySelector('span:[first-child]')
     const up = document.querySelector('span[data-up]')
 
     if (up) {
